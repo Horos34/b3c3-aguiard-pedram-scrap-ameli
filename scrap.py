d4a728ef742c3b6cc339906fe39ad7b9b00993ef
Pedram <aguiardpedram@gmail.com>
2023-02-07 13:57:12 +0000

    reussite recuperation medecin dans l'herault

---
+++ scrap.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "http://annuairesante.ameli.fr/professionnels-de-sante/recherche/liste-resultats-page-1-par_page-20-tri-aleatoire.html"
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+}
+payload = {
+    "type": "ps",
+    "ps_profession": "34",
+    "ps_profession_label" : "Médecin généraliste",
+    "ps_sexe": "2",
+    "ps_localisation" : "HERAULT (34)",
+    "localisation_category": "departements",
+    "submit_final": "Rechercher"
+}
+
+req = requests.Session()
+page = req.post(url,payload,header)
+
+if page.status_code == 200:
+    linkSearch = page.url
+
+soup = BeautifulSoup(page.text, "html.parser")
+
+print(soup)
